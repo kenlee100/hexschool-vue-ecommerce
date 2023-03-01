66d62e9227c2792ad43dac4c11a1e20e894fed0f
kenlee <larukuhyde05@gmail.com>
2023-03-02 00:04:38 +0800

    feat: 調整RWD

--- src/components/front/FooterNav.vue
+++ src/components/front/FooterNav.vue
@@ -2,14 +2,14 @@
   <footer class="bg-primary-primary-100">
     <div class="container">
       <div
-        class="flex items-center justify-between py-[54px] border-t border-netural-netural-500"
+        class="flex flex-col md:flex-row md:items-center md:justify-between py-20 md:py-[54px] border-t border-netural-netural-500"
       >
-        <p class="en-title">JAPAN TRIP</p>
-        <nav>
-          <ul class="flex items-center space-x-[86px]">
+        <p class="pb-[26px] md:pb-0 whitespace-nowrap en-body md:en-title">JAPAN TRIP</p>
+        <nav class="">
+          <ul class="flex items-center  md:space-x-6 lg:space-x-[86px]">
             <li v-for="item in navList" :key="item.path">
               <router-link
-                class="block p-3 font-bold ch-body text-netural-netural-400 hover:text-secondary-secondary-200"
+                class="block py-[12px] px-1 md:p-3 font-bold ch-body text-netural-netural-400 hover:text-secondary-secondary-200"
                 :to="item.path"
                 >{{ item.name }}</router-link
               >
@@ -21,7 +21,7 @@
 
     <div class="bg-netural-netural-500">
       <div class="container">
-        <p class="py-6 md:px-3 ch-body text-center text-netural-netural-100">
+        <p class="py-3 md:py-6 md:px-3 ch-caption-2 md:ch-body text-center text-netural-netural-100">
           本網站僅供個人作品使用，不提供商業用途
         </p>
       </div>
--- src/components/front/HeaderNav.vue
+++ src/components/front/HeaderNav.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
     <div class="container">
-      <div class="flex items-center justify-between py-4">
-        <router-link to="/" class="text-heading-3 font-bold font-antonio"
+      <div class="flex items-center justify-between py-5 lg:py-4">
+        <router-link to="/" class="whitespace-nowrap en-body"
           >JAPAN TRIP</router-link
         >
-        <div class="flex items-center space-x-24">
-          <nav>
+        <div class="flex items-center md:space-x-6 lg:space-x-24">
+          <nav class="hidden md:block">
             <ul class="flex items-center space-x-10">
               <li v-for="item in navList" :key="item.path">
                 <router-link
@@ -17,13 +17,20 @@
               </li>
             </ul>
           </nav>
-          <div class="flex space-x-8">
+          <div class="flex space-x-2">
             <div
-              class="flex-shrink-0 w-5 h-5 bg-secondary-secondary-100 cursor-pointer icon-favorite hover:icon-favorite-solid"
-            ></div>
-            <div
-              class="flex-shrink-0 w-5 h-5 bg-secondary-secondary-100 cursor-pointer icon-cart"
-            ></div>
+              class="flex items-center justify-center w-12 h-12 cursor-pointer"
+            >
+              <div
+                class="flex-shrink-0 w-5 h-5 bg-secondary-secondary-200 transition-all ease-in-out duration-300 icon-favorite hover:icon-favorite-solid"
+              ></div>
+            </div>
+            <div class="flex items-center justify-center w-12 h-12 cursor-pointer">
+              <div
+                class="flex-shrink-0 w-5 h-5 bg-secondary-secondary-200 icon-cart"
+              ></div>
+
+            </div>
           </div>
         </div>
       </div>
