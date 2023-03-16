343655f083fba090581446569b903d0c07c6ff03
kenlee <larukuhyde05@gmail.com>
2023-03-17 00:58:50 +0800

    feat: 更新sweet alert

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -3,7 +3,7 @@ import Swal from "sweetalert2";
 export default Swal.mixin({
   toast: true,
   showConfirmButton: false,
-//   timer: 1500,
+  timer: 1500,
   position: "top-end",
   showClass: {
     popup: "animate__animated animate__slideInRight",
